e1f18bf0c3f403c2ebac7ada79561dce99d244f3
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-11-26 12:27:17 +0800

    add js code

--- node/test.js
+++ node/test.js
@@ -1,3 +1,19 @@
 width = 320;
 width <<= 1;
 console.log(width);
+
+function A() {
+	this.buffs = [];
+}
+
+var a = new A();
+var b = new A();
+var c = new A();
+
+a.buffs = [{
+	buffId: 1
+}];
+
+console.log(a);
+console.log(b);
+console.log(c);
